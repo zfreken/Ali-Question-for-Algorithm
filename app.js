ac42d46f6f4c81b45d6a5b01424cacbfd24b3fa1
Zafer EKEN <zafereken@Zafer-MacBook-Air.local>
2020-03-01 18:43:20 +0300

    code modified for ES6 syntax

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-function mahmut_question(N,K) {
+const gameofgamble = (n,k) => {
 
-  var K,N,ai_count=0,c_count=0;
+  let ai_count=0,c_count=0;
 
-  while (N != 1) {
-    if (N%2 == 0 && ai_count < K) {
-      N = N/2;
+  while (n > 1) {
+    if (n%2 == 0 && ai_count < k) {
+      n = n/2;
       ai_count++;
     }else{
-      N = N-1;
+      n--;
       c_count++;
     }
   }
-
   console.log('All in Oynanan El Sayısı:',ai_count);
   console.log('Chip Para Oynanan El Sayısı:',c_count);
 
   return ai_count + c_count;
-
 }
 
 
-console.log('Toplam El Sayısı:',mahmut_question(18,2));
+console.log('Toplam El Sayısı:',gameofgamble(18,2));
 console.log('---------------------------------');
-console.log('Toplam El Sayısı:',mahmut_question(8,0));
+console.log('Toplam El Sayısı:',gameofgamble(8,0));
 console.log('---------------------------------');
-console.log('Toplam El Sayısı:',mahmut_question(10,10));
+console.log('Toplam El Sayısı:',gameofgamble(10,10));
+
+
+
+//Alternative method
+
+// function denk(n, k){
+//   if(k===0 || n === 1) return n-1;
+//   else if(n%2 === 0) return denk(n/2, k-1) + 1;
+//   else return denk(n-1, k) + 1;
+// }
+// console.log(denk(10,10));
+
